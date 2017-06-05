f35a79cdc90841c29b43a27601bc7ea0d6092e90
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-06-05 12:46:59 +0300

    html and images compiling is configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,48 @@
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  context: path.resolve(__dirname, "./src/"),
-  entry: {
-    hw1: "./js/main.js"
-  },
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "js/script.js",
-    publicPath: "/"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "./src"),
-    compress: true,
-    port: 8000,
-    watchContentBase: true,
-    watchOptions: {
-      poll: true
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader", "sass-loader"]
+    context: path.resolve(__dirname, "./src/"),
+    entry: {
+        hw1: "./js/main.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "js/script.js",
+        publicPath: "./"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "./src"),
+        compress: true,
+        port: 8000,
+        watchContentBase: true,
+        watchOptions: {
+            poll: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: "file-loader?name=[path][name].[ext]"
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: "css/style.css",
+            allChunks: true
+        }),
+        new HtmlWebpackPlugin({
+            template: "index.html"
         })
-      }
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: "css/style.css",
-      allChunks: true
-    })
-  ]
 };
