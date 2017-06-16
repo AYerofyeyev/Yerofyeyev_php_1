0ddf5f3be8860632c9dfac25308089b56f25808c
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-06-16 16:13:59 +0300

    menu is added

--- src/js/components/menu.jsx
+++ src/js/components/menu.jsx
@@ -8,13 +8,52 @@ class Menu extends React.Component {
 
     render(){
         return(
-            <ul>
-                <li>
-                    <a>
-                        Home
-                    </a>
-                </li>
-            </ul>
+            <nav>
+                <ul>
+                    <li>
+                        <a>
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Articles:
+                        </a>
+                        <ul>
+                            <li>
+                                <a>
+                                    Article1
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Article2
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Article3
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Article4
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Article5
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a>
+                            Contacts
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         );
     }
 }
